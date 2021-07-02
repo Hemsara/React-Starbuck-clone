c317f663bb654a36cac79397960689c305d3e0f8
Vehan <contact.vehanhemsara@gmail.com>
2021-07-02 02:22:54 -0700

    added responsive styles for hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,14 +3,16 @@ import ImageArea from "./ImageArea";
 import "./Hero.css";
 
 const Hero = () => {
-    return ( 
-        <div className="hero-wrapper">
-              <TextArea heading={"FREE COFFEE IS A TAP WAY"} 
-              para={"Join now to start earning Rewards."}       />
-              
-              <ImageArea />
-        </div>
-     );
-}
- 
+  return (
+    <div className="hero-wrapper">
+      <TextArea
+        heading={"FREE COFFEE IS A TAP WAY"}
+        para={"Join now to start earning Rewards."}
+      />
+
+      <ImageArea />
+    </div>
+  );
+};
+
 export default Hero;
