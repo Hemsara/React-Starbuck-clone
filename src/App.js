5c3c00a320acbbf47051894cf7a306e1a45fe33d
Vehan <contact.vehanhemsara@gmail.com>
2021-07-02 00:25:52 -0700

    styled covid message section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Message from "./components/Message";
 
 function App() {
   return (
     <div className="App">
        <Navbar />
+       <Message />
     </div>
   );
 }
---
+++ src/components/Message.js
@@ -0,0 +1,12 @@
+import "./Message.css";
+
+
+const Message = () => {
+    return ( 
+        <div className="message">
+            <p>Facial coverings are now optional in our stores for our fully vaccinated customers, following CDC guidance, except where local regulations require it by law. Learn more</p>
+        </div>
+     );
+}
+ 
+export default Message;
