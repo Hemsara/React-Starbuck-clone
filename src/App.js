3dffc2a9b0bd66d72e9c6c4a775fccd33b06af42
Vehan <contact.vehanhemsara@gmail.com>
2021-07-05 16:59:46 -0700

    fixed border box

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import Message from "./components/Message";
 import Hero from "./components/Hero";
 import Order from "./components/Order";
 import Online from "./components/Online";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-       <Navbar />
-       <Message />
-       <Hero />
-       <Order />
-       <Online />
-
+      <Navbar />
+      <Message />
+      <Hero />
+      <Order />
     </div>
   );
 }
